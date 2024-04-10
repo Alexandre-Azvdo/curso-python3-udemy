4fc1d891494c556ad0f0e05e14a25f4f00e9944e
Alexandre Azevedo <azvdo.ads@gmail.com>
2024-04-10 19:59:12 -0300

    Aula 1: Formas de Comentario e DocStrings

---
+++ aula1.py
@@ -0,0 +1,14 @@
+# Comentário de uma linha
+print('Hello, world!') # Escreve no terminal o conteúdo que está entre parenteses
+
+"""
+DocString
+Forma de documentar o código (PS: Não tenho certeza)
+
+"""
+
+'''
+DocString
+Outra forma
+
+'''
