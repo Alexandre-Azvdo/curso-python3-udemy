dbfe554c78d3b2457dcec79d71b8b5611296b5a3
Alexandre Azevedo <azvdo.ads@gmail.com>
2024-04-13 08:38:03 -0300

    Aula 3: Tipo str (string) - Introducao aos tipos de dados

---
+++ aula3.py
@@ -0,0 +1,23 @@
+""" (DocString)
+ Python = Linguagem de programação 
+ Tipo de tipagem = Dinâmica / Forte
+ str -> string -> texto
+ Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Alexandre Azevedo')
+
+# Aspas duplas
+print("Alexandre Azevedo")
+
+# Escape
+print("Alexandre \"Azevedo\"")
+
+# r (serve para expressões regulares, geralmente)
+print(r"Alexandre \"Azevedo\"")
+
+# Outras combinações
+print("'Alexandre Azevedo'")
+print('"Alexandre Azevedo"')
