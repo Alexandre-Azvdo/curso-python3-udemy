c0d11f6100bd2f236fe1bec7ec0d7cfc93c73a16
Alexandre Azevedo <azvdo.ads@gmail.com>
2024-04-17 09:30:22 -0300

    Aula 8: Exercicio com variaveis e tipos de dados

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Alexandre'
+sobrenome = 'Azevedo Rodrigues dos Santos'
+idade = 38
+ano_nascimento = 1985
+maior_idade = idade >= 18
+altura_metros = 1.71
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros)
