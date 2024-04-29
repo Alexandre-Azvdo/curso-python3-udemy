fafc6cc495061bf3406c1f634beea5a2ea1c9e21
Alexandre Azevedo <azvdo.ads@gmail.com>
2024-04-28 23:44:08 -0300

    Aula 24: Operadores in e not in

---
+++ aula24.py
@@ -0,0 +1,14 @@
+# Operadores in e not in
+# Strings são iteráveis
+#  0  1  2  3  4  5 
+#  o  t  á  v  i  o
+# -6 -5 -4 -3 -2 -1
+nome = 'Otávio'
+# print(nome[2])
+# print(nome[-4])
+
+print('vio' in nome)
+print('z' in nome)
+print('-' * 10)
+print('á' not in nome)
+print('z' not in nome)
