e0d28c6b8347d38c74f4f111757c53ea0e8a587a
Alexandre Azevedo <azvdo.ads@gmail.com>
2024-05-11 12:37:04 -0300

    Aula 48: Tipo list - Introducao as listas mutaveis em Python

---
+++ aula48.py
@@ -0,0 +1,16 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+#        +01234
+#        -54321
+string = 'ABCDE' # 5 caracteres (len)
+
+#         0     1            2            3   4
+#        -5    -4           -3           -2   -1
+lista = [123, True, 'Alexandre Azevedo', 1.2, []] 
+lista[-3] = 'Alexandre_Azvdo'
+print(lista[2], type(lista[2]))
