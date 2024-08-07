a436bf38d8a3ed2819d0042a0f97a28db381a5ea
Alexandre Azevedo <azvdo.ads@gmail.com>
2024-08-07 09:27:33 -0300

    Aula 117: Variaveis livres + nonlocal (locals, globals) - Update aula117.py

--- aula117.py
+++ aula117.py
@@ -15,15 +15,12 @@ def criar_funcao(func):
         return resultado
     return interna
 
-
 def inverte_string(string):
     return string[::-1]
-
 
 def e_string(param):
     if not isinstance(param, str):
         raise TypeError('param deve ser uma string')
-
 
 inverte_string_checando_parametro = criar_funcao(inverte_string)
 invertida = inverte_string_checando_parametro('123')
