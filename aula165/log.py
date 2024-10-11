589fe6790daccef1cdbaa60f0c11a1d8993277ae
Alexandre Azevedo <azvdo.ads@gmail.com>
2024-10-11 08:03:42 -0300

    Aula 165: (Parte 2) Log, LogFileMixin, LogPrintMixin e a uniao de tudo ate aqui

--- aula165/log.py
+++ aula165/log.py
@@ -1,17 +1,28 @@
 # Abstração
 class Log:
 
-    def log(self, msg):
+    def _log(self, msg):
         raise  NotImplementedError('Implemente o método de log')
     
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
 
 class LogFileMixin(Log):
 
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
 
 
+class LogPrintMixin(Log):
+
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
 
 if __name__ == '__main__': 
-        l = LogFileMixin()
-        l.log('Qualquer coisa')
+        l = LogPrintMixin()
+        l.log_error('Não logado')
+        l.log_success('Logado')
